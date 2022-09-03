6dab2ea5eaaed537400a50f199e671f1ad9f6245
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-09-03 11:18:39 +0530

    end home page

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, View, Text, Image } from "react-native";
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex:1}}>
       <ScrollView>
-
+        <View style={styles.imageHome}>
+          <Image source={require('../assets/images/home_bg.png')} resizeMode='contain' style={{height:200}}/>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.myTitle}>
+            Welcome To My First App
+          </Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'white',
+  },
+  myTitle:{
+    fontSize:24,
+    fontWeight:'bold',
+    textAlign:'center'
+  },
+  imageHome:{
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
 
 export default Home;
