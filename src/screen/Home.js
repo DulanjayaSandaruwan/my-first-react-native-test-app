b1a18683337d6e0190e812f488150953414af007
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-09-03 10:57:53 +0530

    first commit

---
+++ src/screen/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const style = StyleSheet.create({
+
+});
+
+export default Home;
